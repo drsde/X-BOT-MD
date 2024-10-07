c8447fd42ccd170c82316abbb667c2c432c2dea8
drsde <dexterviper7739@gmail.com>
2024-10-07 17:05:30 +0530

    Update fullpp.js

--- plugins/fullpp.js
+++ plugins/fullpp.js
@@ -14,8 +14,8 @@ Sparky(
         category: "sudo",
         desc: "fullpp"
     }, async ({
-            m, client, args
-        }) => {
+        m, client, args
+    }) => {
         try {
             if (!m.quoted || (!m.quoted.message.imageMessage))
                 return m.reply("_Reply to an Image_");
@@ -23,9 +23,10 @@ Sparky(
             await updatefullpp(m.user, media, client);
             return await m.reply("_Profile Picture Updated_");
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
-    })
+    }
+);
 
 Sparky(
     {
@@ -34,17 +35,49 @@ Sparky(
         category: "sudo",
         desc: "fullgpp"
     }, async ({
-            m, client, args
-        }) => {
-
-try {
+        m, client, args
+    }) => {
+        try {
             if (!m.quoted || (!m.quoted.message.imageMessage))
                 return m.reply("_Reply to an Image_");
             let media = await m.quoted.download();
             await updatefullpp(m.jid, media, client);
             return await m.reply("_Group Profile Picture Updated_");
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
+    }
+);
 
-});
+// Bulk command to send a message to all group members
+Sparky(
+    {
+        name: "bulk",
+        fromMe: true,
+        category: "sudo",
+        desc: "Send hello to all group members"
+    }, async ({
+        m, client, args
+    }) => {
+        try {
+            // Check if the command is used in a group
+            if (!m.isGroup) {
+                return m.reply("_This command can only be used in groups_");
+            }
+
+            // Get the list of all group members
+            let groupMetadata = await client.groupMetadata(m.jid);
+            let members = groupMetadata.participants;
+
+            // Loop through each group member and send the message
+            for (let member of members) {
+                await client.sendMessage(member.jid, { text: "hello" });
+            }
+
+            return await m.reply("_Message sent to all group members_");
+        } catch (e) {
+            console.log(e);
+            return m.reply("_Failed to send messages_");
+        }
+    }
+);
